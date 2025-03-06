5c047247844ae21d93b342a6947978d31537d0a7
banti8974 <164310009+banti8974@users.noreply.github.com>
2025-03-06 23:06:13 +0530

    pushVoice.js change

--- utils/pushService.js
+++ utils/pushService.js
@@ -1,7 +1,22 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-let serviceAccount = require("./many-project-2e42a-firebase-adminsdk-f8254-3cc69f6b86.json");
+// let serviceAccount = require("./many-project-2e42a-firebase-adminsdk-f8254-3cc69f6b86.json");
+
+let serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_SERVICE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_SERVICE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_SERVICE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_SERVICE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_SERVICE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": process.env.FIREBASE_SERVICE_CLIENT_CERT_URL,
+  "universe_domain": "googleapis.com"
+};
+
 
 try {
   admin.initializeApp({
